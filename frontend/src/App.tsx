0c51e1c377b607129a08794d3b7860979bc71f18
root_who <joaopaulo_gonzaga@outlook.com>
2021-09-06 19:09:05 -0300

    Adicionando BootStrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 
-
-import './App.css';
-
 function App() {
   return (
           <>
-          <h1>Ola Mundo!</h1>
+          <h1 className="text-primary">Olá Mundo!</h1>
           </>  
   );
 }

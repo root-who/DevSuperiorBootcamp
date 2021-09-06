a1f0e851939b4bfa3d0df51c28f94c35a6a4c99d
root_who <joaopaulo_gonzaga@outlook.com>
2021-09-06 18:38:46 -0300

    first commit SpringBoot - React - DevSuperior

---
+++ backend/src/test/java/com/devsuperior/dsvends/DsvendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

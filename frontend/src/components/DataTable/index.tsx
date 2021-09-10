7da26be07f28e73906a06d0f4cc6826eafcc98c0
root_who <joaopaulo_gonzaga@outlook.com>
2021-09-10 12:32:46 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,8 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sales";
+import { formatLocalDate } from "utils/formatData";
+import { baseUrl } from "utils/requests";
+
+
+
 function DataTable(){
+
+    const [tableData , setTableData] = useState<SalePage>({
+        first: true,
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        number: 0,
+    });
+
+    function getDataFromServer(){
+        axios.get(`${baseUrl}/sales?page=0&size=10&sort=date,desc`)
+             .then((response)=>{
+                    const data = response.data as SalePage;
+                    const myContent = data.content;
+                    const totalOfPages = data.totalPages;
+                    const totalOfElements = data.totalElements;
+                    const theNumber = data.number;
+                    setTableData({
+                                content:myContent,
+                                 first:true, last:true, totalPages : totalOfPages,
+                                 totalElements:totalOfElements, number:theNumber
+                                 })
+             })
+    }
+
+     useEffect(getDataFromServer,[]);
+
     return (
         <>
             <div className="table-responsive mt-5">
-                <table className="table table-striped table-sm">
+                <table className="table table-striped table-sm mb-2">
                     <thead>
                         <tr>
                             <th>Data</th>
@@ -13,48 +48,19 @@ function DataTable(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>22/04/2021</td>
-                            <td>Barry Allen</td>
-                            <td>34</td>
-                            <td>25</td>
-                            <td>15017.00</td>
-                        </tr>
-                        <tr>
-                            <td>22/04/2021</td>
-                            <td>Barry Allen</td>
-                            <td>34</td>
-                            <td>25</td>
-                            <td>15017.00</td>
-                        </tr>
-                        <tr>
-                            <td>22/04/2021</td>
-                            <td>Barry Allen</td>
-                            <td>34</td>
-                            <td>25</td>
-                            <td>15017.00</td>
-                        </tr>
-                        <tr>
-                            <td>22/04/2021</td>
-                            <td>Barry Allen</td>
-                            <td>34</td>
-                            <td>25</td>
-                            <td>15017.00</td>
-                        </tr>
-                        <tr>
-                            <td>22/04/2021</td>
-                            <td>Barry Allen</td>
-                            <td>34</td>
-                            <td>25</td>
-                            <td>15017.00</td>
-                        </tr>
-                        <tr>
-                            <td>22/04/2021</td>
-                            <td>Barry Allen</td>
-                            <td>34</td>
-                            <td>25</td>
-                            <td>15017.00</td>
-                        </tr>
+                        {
+                        tableData.content?.map((value, index)=>{
+                            return(
+                                <tr key={index}>
+                                    <td>{formatLocalDate(value.date, "dd/MM/yyyy")}</td>
+                                    <td>{value.seller.name}</td>
+                                    <td>{value.visitedClients}</td>
+                                    <td>{value.dealsMaked}</td>
+                                    <td>{value.amount.toFixed(2)}</td>
+                                </tr>
+                            )
+                        })
+                        }
                     </tbody>
                 </table>
             </div>
--- frontend/src/types/sales.ts
+++ frontend/src/types/sales.ts
@@ -8,3 +8,29 @@ export type SellerSuccess = {
         clientsVisited: number,
         dealsMaked: number
 }
+
+export type Sale ={
+    id: number,
+    date: string,
+    visitedClients: number,
+    dealsMaked: number,
+    amount: number,
+    seller:Seller
+}
+
+export type SalePage ={
+    content?: Sale[];
+    last: boolean,
+    totalPages: number,
+    totalElements: number,
+    size?: number,
+    number: number,
+    first: boolean,
+    numberOfElements?: number,
+    empty?: boolean
+}
+
+export type Seller = {
+    id: number,
+    name: string
+}

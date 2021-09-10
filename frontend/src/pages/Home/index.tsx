a205c9b5dfb40fdf0faf38c6b5de539e80287974
root_who <joaopaulo_gonzaga@outlook.com>
2021-09-10 10:13:56 -0300

    Home and Navagation with routes

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
-function Home (){
-    return(
+import Footer from "components/Footer"
+import NavBar from "components/HeaderNavBar"
+import { Link } from "react-router-dom"
+
+function Home() {
+    return (
         <>
-        <h1>Home</h1>
+            <NavBar/>
+            <div className="container mt-5">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <Link className="mt-2 btn btn-primary btn-lg" to="/dashboard">Acessar o DashBoard</Link>
+                </div>
+            </div>
+            <Footer/>
         </>
     )
 }

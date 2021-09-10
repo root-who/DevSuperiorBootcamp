430a5d6edfa598f9b43b497602fcf387eef62cd9
root_who <joaopaulo_gonzaga@outlook.com>
2021-09-10 10:02:42 -0300

    Routes adicionado no React

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,31 @@
+import DonutChart from "components/DonutChart/index";
+import BarChart from "components/BarChart/index";
+import DataTable from "components/DataTable/index";
+import Footer from "components/Footer/index";
+import NavBar from "components/HeaderNavBar/index";
+
+const Dashboard = () =>{
+    return(
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+                
+                <div className="row px-3 mt-5">
+                <div className="col-sm-6">
+                    <h5>Taxa de Sucesso (%)</h5>
+                    <BarChart/>
+                </div>
+                <div className="col-sm-6">
+                    <h5>Todas as Vendas</h5>
+                    <DonutChart/>
+                </div>
+                </div>       
+                <DataTable/> 
+            </div>
+            <Footer/>
+          </>
+    )
+}
+
+export default Dashboard;

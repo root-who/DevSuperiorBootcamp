5c230bec110dc8a6b87e1e337c1e6e0f4610bd9b
root_who <joaopaulo_gonzaga@outlook.com>
2021-09-10 11:24:37 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,22 +1,28 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sales';
 import { baseUrl } from 'utils/requests';
 
 type ChartData = {
     labels: string[];
-    series:number[];
+    series: number[];
 }
 
 const DonutChart = () =>{
 
-    let chartData : ChartData ={ 
-        labels:[], 
-        series:[]
-    };
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series:[]});
+    
+    useEffect(()=>{
+        axios.get(`${baseUrl}/sales/sumamount-by-seller`)
+                .then((response)=>{
+                    const data = response.data as SaleSum[];
+                    const mylabels = data.map(x=>x.sellerName);
+                    const myAmount = data.map(x => x.sumAmountSale)
+                    setChartData({labels : mylabels, series: myAmount})
+                })
+    }, []);
 
-    axios.get(`${baseUrl}/sales/sumamount-by-seller`).then((response)=>{
-            chartData = response.data;     
-        })
 
     // const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
--- frontend/src/types/sales.ts
+++ frontend/src/types/sales.ts
@@ -1,4 +1,4 @@
 export type SaleSum = {
     sellerName: string,
-    sumAmountSale: string
+    sumAmountSale: number
 }
